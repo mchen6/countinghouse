90ccaf87b29c4e9b00688b4e41155ff0ee375d0e
out4b <stavrogin6@gmail.com>
2016-09-14 15:47:50 +0800

    support for global db path under /var/lib/cdif

--- framework.js
+++ framework.js
@@ -3,9 +3,11 @@ var RouteManager  = require('./lib/route-manager');
 var argv          = require('minimist')(process.argv.slice(1));
 var options       = require('./lib/cli-options');
 var logger        = require('./lib/logger');
+var deviceDB      = require('./lib/device-db');
 
 options.setOptions(argv);
 logger.createLogger();
+deviceDB.init();
 
 var mm = new ModuleManager();
 var routeManager = new RouteManager(mm);
