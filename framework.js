4d9c62bfecb13d5c47b0dee76b15eaa3a19d9649
out4b <stavrogin6@gmail.com>
2016-09-14 18:03:49 +0800

    fix logger uninit error on startup

--- framework.js
+++ framework.js
@@ -5,8 +5,8 @@ var options       = require('./lib/cli-options');
 var logger        = require('./lib/logger');
 var deviceDB      = require('./lib/device-db');
 
-options.setOptions(argv);
 logger.createLogger();
+options.setOptions(argv);
 deviceDB.init();
 
 var mm = new ModuleManager();
