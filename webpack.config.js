d7131f999fa70f3c08f35f900e537cd09570d8d8
out4b <stavrogin6@gmail.com>
2017-06-24 15:15:04 +0800

    tweak for webpack production code

---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+var StringReplacePlugin = require("string-replace-webpack-plugin");
+var JavaScriptObfuscator = require('webpack-obfuscator');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  module: {
+    rules: [
+      // { test: /.*\.js$/,
+
+      //       loader: StringReplacePlugin.replace({
+      //           replacements: [
+      //               {
+      //                   pattern: /var.*CdifDevice.*\=.*require.*cdif-device\'\)\;/ig,
+      //                   replacement: function (match, p1, offset, string) {
+      //                       return '';
+      //                   }
+      //               },
+      //               {
+      //                   pattern: /var.*CdifError.*\=.*require.*cdif-error\'\)\.CdifError\;/ig,
+      //                   replacement: function (match, p1, offset, string) {
+      //                       return '';
+      //                   }
+      //               },
+      //               {
+      //                   pattern: /var.*DeviceError.*\=.*require.*cdif-error\'\)\.DeviceError\;/ig,
+      //                   replacement: function (match, p1, offset, string) {
+      //                       return '';
+      //                   }
+      //               },
+      //               {
+      //                   pattern: /var.*CdifUtil.*\=.*require.*cdif-util\'\)\;/ig,
+      //                   replacement: function (match, p1, offset, string) {
+      //                       return '';
+      //                   }
+      //               }
+      //           ]
+      //       })
+      // },
+      { test: require.resolve("./lib/cdif-util.js"), loader: "expose-loader?CdifUtil" },
+      { test: require.resolve("./lib/cdif-device.js"), loader: "expose-loader?CdifDevice"},
+      { test: require.resolve("./lib/cdif-error.js").DeviceError, loader: "expose-loader?DeviceError" }
+    ]
+  },
+  entry: './framework.js',
+  target: 'node',
+  node: {
+    __dirname: true
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  externals: nodeModules,
+  plugins: [
+    // new webpack.optimize.UglifyJsPlugin({
+    //     compress: {
+    //         warnings: false
+    //     }
+    // }),
+    // new JavaScriptObfuscator({
+    //     rotateUnicodeArray: true
+    // }, [])
+  ]
+}
