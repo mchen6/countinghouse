dde031292ef05b0e9d24959f8547e3e1dfb9954c
out4b <stavrogin6@gmail.com>
2017-07-10 13:51:32 +0800

    update wetty to new code repo: https://github.com/butlerx/wetty

--- wetty/README.md
+++ wetty/README.md
@@ -11,19 +11,24 @@ hterm source - https://chromium.googlesource.com/apps/libapps/+/master/hterm/
 
 ![Wetty](/terminal.png?raw=true)
 
-Install
--------
+This fork has a few of the open PR's from the original merged in as well as scripts to make running
+in docker better.
 
-*  `git clone https://github.com/krishnasrinivas/wetty`
+## Install
 
-*  `cd wetty`
+- `git clone https://github.com/butlerx/wetty`
+- `cd wetty`
+- `yarn`
 
-*  `npm install`
+or
 
-Run on HTTP:
------------
+`yarn add wetty.js`
 
-    node app.js -p 3000
+## Run on HTTP
+
+``` bash
+node app.js -p 3000
+```
 
 If you run it as root it will launch `/bin/login` (where you can specify
 the user name), else it will launch `ssh` and connect by default to
@@ -35,77 +40,112 @@ SSH user using the `--sshuser` option.
 
 You can also specify the SSH user name in the address bar like this:
 
-  `http://yourserver:3000/wetty/ssh/<username>`
+`http://yourserver:3000/wetty/ssh/<username>`
 
-Run on HTTPS:
-------------
+or
 
-Always use HTTPS! If you don't have SSL certificates from a CA you can
+`http://yourserver:3000/ssh/<username>`
+
+## Run on HTTPS
+
+Always use HTTPS. If you don't have SSL certificates from a CA you can
 create a self signed certificate using this command:
 
-  `openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 30000 -nodes`
+```
+openssl req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 30000 -nodes
+```
 
 And then run:
 
-    node app.js --sslkey key.pem --sslcert cert.pem -p 3000
+```
+node app.js --sslkey key.pem --sslcert cert.pem -p 3000
+```
 
 Again, if you run it as root it will launch `/bin/login`, else it will
 launch SSH to `localhost` or a specified host as explained above.
 
-Run wetty behind nginx:
-----------------------
+## Run wetty behind nginx
 
 Put the following configuration in nginx's conf:
 
     location /wetty {
-	    proxy_pass http://127.0.0.1:3000/wetty;
-	    proxy_http_version 1.1;
-	    proxy_set_header Upgrade $http_upgrade;
-	    proxy_set_header Connection "upgrade";
-	    proxy_read_timeout 43200000;
+      proxy_pass http://127.0.0.1:3000/wetty;
+      proxy_http_version 1.1;
+      proxy_set_header Upgrade $http_upgrade;
+      proxy_set_header Connection "upgrade";
+      proxy_read_timeout 43200000;
 
-	    proxy_set_header X-Real-IP $remote_addr;
-	    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-	    proxy_set_header Host $http_host;
-	    proxy_set_header X-NginX-Proxy true;
+      proxy_set_header X-Real-IP $remote_addr;
+      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+      proxy_set_header Host $http_host;
+      proxy_set_header X-NginX-Proxy true;
     }
 
 If you are running `app.js` as `root` and have an Nginx proxy you have to use:
 
-    http://yourserver.com/wetty
+```
+http://yourserver.com/wetty
+```
 
-Else if you are running `app.js` as a regular user you have to use:
+Else if you are running `app.js` as a regular user you can use:
 
-    http://yourserver.com/wetty/ssh/<username>
+```
+http://yourserver.com/wetty/ssh/<username>
+```
+
+or
+
+```
+http://yourserver.com/wetty
+```
 
 **Note that if your Nginx is configured for HTTPS you should run wetty without SSL.**
 
-Dockerized Version
-------------------
+## Dockerized Version
 
-This repo includes a Dockerfile you can use to run a Dockerized version of wetty.  You can run
+This repo includes a Dockerfile you can use to run a Dockerized version of wetty. You can run
 whatever you want!
 
-Just do:
+Just modify docker-compose and run:
 
 ```
-    docker run --name term -p 3000 -dt nathanleclaire/wetty
+docker-compose up -d
 ```
 
-Visit the appropriate URL in your browser (`[localhost|$(boot2docker ip)]:PORT`).  
-The username is `term` and the password is `term`.
+Visit the appropriate URL in your browser (`[localhost|$(boot2docker ip)]:PORT`).
 
-Run wetty as a service daemon
------------------------------
+The default username is `term` and the password is `term`, if you did not modify `SSHHOST`
 
-Install wetty globally with -g option:
+If you dont want  to build the image yourself just remove the line `build; .`
+
+## Run wetty as a service daemon
+
+Install wetty globally with global option:
+
+### init.d
 
 ```bash
-    $ sudo npm install wetty -g
-    $ sudo cp /usr/local/lib/node_modules/wetty/bin/wetty.conf /etc/init
-    $ sudo start wetty
+$ sudo yarn global add wetty.js
+$ sudo cp /usr/local/lib/node_modules/wetty.js/bin/wetty.conf /etc/init
+$ sudo start wetty
 ```
 
-This will start wetty on port 3000. If you want to change the port or redirect stdout/stderr you should change the last line in `wetty.conf` file, something like this:
+### systemd
 
-    exec sudo -u root wetty -p 80 >> /var/log/wetty.log 2>&1
+```bash
+$ yarn global add wetty.js
+$ cp ~/.config/yarn/global/node_modules/wetty.js/bin/wetty.service  ~/.config/systemd/user/
+$ systemctl --user enable wetty
+$ systemctl --user start wetty
+```
+
+This will start wetty on port 3000. If you want to change the port or redirect
+stdout/stderr you should change the last line in `wetty.conf` file, something
+like this:
+```
+exec sudo -u root wetty -p 80 >> /var/log/wetty.log 2>&1
+```
+## Clean-up
+
+If users dont fully disconnect when finished ssh connections will actually be kept open the simplest
+way to deal with this is run `bin/cleanup` on a cron job.
--- wetty/app.js
+++ wetty/app.js
@@ -1,126 +1,156 @@
 var express = require('express');
-var http = require('http');
-var https = require('https');
 var path = require('path');
 var server = require('socket.io');
 var pty = require('pty.js');
 var fs = require('fs');
 
-// var opts = require('optimist')
-//     .options({
-//         sslkey: {
-//             demand: false,
-//             description: 'path to SSL key'
-//         },
-//         sslcert: {
-//             demand: false,
-//             description: 'path to SSL certificate'
-//         },
-//         sshhost: {
-//             demand: false,
-//             description: 'ssh server host'
-//         },
-//         sshport: {
-//             demand: false,
-//             description: 'ssh server port'
-//         },
-//         sshuser: {
-//             demand: false,
-//             description: 'ssh user'
-//         },
-//         sshauth: {
-//             demand: false,
-//             description: 'defaults to "password", you can use "publickey,password" instead'
-//         },
-//         port: {
-//             demand: true,
-//             alias: 'p',
-//             description: 'wetty listen port'
-//         },
-//     }).boolean('allow_discovery').argv;
+let globalsshuser = process.env.SSHUSER || '';
+let sshhost = process.env.SSHHOST || 'localhost';
+let sshport = process.env.SSHPOST || 22;
+let sshauth = process.env.SSHAUTH || 'password';
 
-// var runhttps = false;
-// var sshport = 22;
-// var sshhost = 'localhost';
-// var sshauth = 'password';
-// var globalsshuser = '';
+// const opts = require('optimist')
+//   .options({
+//     sslkey: {
+//       demand     : false,
+//       description: 'path to SSL key',
+//     },
+//     sslcert: {
+//       demand     : false,
+//       description: 'path to SSL certificate',
+//     },
+//     sshhost: {
+//       demand     : false,
+//       description: 'ssh server host',
+//     },
+//     sshport: {
+//       demand     : false,
+//       description: 'ssh server port',
+//     },
+//     sshuser: {
+//       demand     : false,
+//       description: 'ssh user',
+//     },
+//     sshauth: {
+//       demand     : false,
+//       description: 'defaults to "password", you can use "publickey,password" instead',
+//     },
+//     port: {
+//       demand     : false,
+//       alias      : 'p',
+//       description: 'wetty listen port',
+//     },
+//   })
+//   .boolean('allow_discovery').argv;
+
+// let runhttps = process.env.HTTPS || false;
+// let sshhost = process.env.SSHHOST || 'localhost';
+// let sshauth = process.env.SSHAUTH || 'password';
+// let sshport = process.env.SSHPOST || 22;
+// let port = process.env.PORT || 3000;
 
 // if (opts.sshport) {
-//     sshport = opts.sshport;
+//   sshport = opts.sshport;
 // }
 
 // if (opts.sshhost) {
-//     sshhost = opts.sshhost;
+//   sshhost = opts.sshhost;
 // }
 
 // if (opts.sshauth) {
-// 	sshauth = opts.sshauth
+//   sshauth = opts.sshauth;
 // }
 
 // if (opts.sshuser) {
-//     globalsshuser = opts.sshuser;
+//   globalsshuser = opts.sshuser;
+// }
+
+// if (opts.port) {
+//   port = opts.port;
 // }
 
 // if (opts.sslkey && opts.sslcert) {
-//     runhttps = true;
-//     opts['ssl'] = {};
-//     opts.ssl['key'] = fs.readFileSync(path.resolve(opts.sslkey));
-//     opts.ssl['cert'] = fs.readFileSync(path.resolve(opts.sslcert));
+//   runhttps = true;
+//   opts['ssl'] = {};
+//   opts.ssl['key'] = fs.readFileSync(path.resolve(opts.sslkey));
+//   opts.ssl['cert'] = fs.readFileSync(path.resolve(opts.sslcert));
 // }
 
-// process.on('uncaughtException', function(e) {
-//     console.error('Error: ' + e);
+// process.on('uncaughtException', e => {
+//   console.error(`Error: ${e}`);
 // });
 
 module.exports = function(httpserv, app) {
-    var sshhost = 'localhost';
-    var globalsshuser = '';
+  app.use(require('serve-favicon')(`${__dirname}/public/favicon.ico`));
+  // For using wetty at /wetty on a vhost
+  app.get('/wetty/ssh/:user', (req, res) => {
+    res.sendfile(`${__dirname}/public/wetty/index.html`);
+  });
+  app.get('/wetty/', (req, res) => {
+    res.sendfile(`${__dirname}/public/wetty/index.html`);
+  });
+  // For using wetty on a vhost by itself
+  app.get('/ssh/:user', (req, res) => {
+    res.sendfile(`${__dirname}/public/wetty/index.html`);
+  });
+  app.get('/', (req, res) => {
+    res.sendfile(`${__dirname}/public/wetty/index.html`);
+  });
+  // For serving css and javascript
+  app.use('/', express.static(path.join(__dirname, 'public')));
 
-    app.get('/wetty/ssh/:user', function(req, res) {
-        res.sendfile(__dirname + '/public/wetty/index.html');
+  const io = server(httpserv, { path: '/wetty/socket.io' });
+  io.on('connection', socket => {
+    let sshuser = '';
+    const request = socket.request;
+    console.log(`${new Date()} Connection accepted.`);
+    const match = request.headers.referer.match('.+/ssh/.+$');
+    if (match) {
+      sshuser = `${match[0].split('/ssh/').pop()}@`;
+    } else if (globalsshuser) {
+      sshuser = `${globalsshuser}@`;
+    }
+    let term;
+    if (process.getuid() === 0 && sshhost === 'localhost') {
+      // this is the path when cdif running inside docker instance
+      term = pty.spawn('/bin/login', ['-f', 'term'], {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 30,
+      });
+    } else if (sshuser) {
+      term = pty.spawn('ssh', [sshuser + sshhost, '-p', sshport, '-o', `PreferredAuthentications=${sshauth}`], {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 30,
+      });
+    } else {
+      //this is the path when running in normal mode
+      term = pty.spawn('ssh', [sshhost, '-p', sshport, '-o', `PreferredAuthentications=${sshauth}`], {
+        name: 'xterm-256color',
+        cols: 80,
+        rows: 30,
+      });
+    }
+
+    console.log(`${new Date()} PID=${term.pid} STARTED on behalf of user=${sshuser}`);
+    term.on('data', data => {
+      socket.emit('output', data);
     });
-    app.use('/', express.static(path.join(__dirname, 'public')));
-
-    var io = server(httpserv,{path: '/wetty/socket.io'});
-    io.on('connection', function(socket){
-        var sshuser = '';
-        var request = socket.request;
-        console.log((new Date()) + ' Connection accepted.');
-        if (match = request.headers.referer.match('/wetty/ssh/.+$')) {
-            sshuser = match[0].replace('/wetty/ssh/', '') + '@';
-        } else if (globalsshuser) {
-            sshuser = globalsshuser + '@';
-        }
-
-        var term;
-        if (process.getuid() == 0) {
-            term = pty.spawn('/bin/login', ['-f', 'term'], {
-                name: 'xterm-256color',
-                cols: 80,
-                rows: 30
-            });
-        } else {
-            term = pty.spawn('ssh', [sshuser + sshhost], {
-                name: 'xterm-256color',
-                cols: 80,
-                rows: 30
-            });
-        }
-        console.log((new Date()) + " PID=" + term.pid + " STARTED on behalf of user=" + sshuser)
-        term.on('data', function(data) {
-            socket.emit('output', data);
-        });
-        term.on('exit', function(code) {
-            console.log((new Date()) + " PID=" + term.pid + " ENDED")
-        });
-        socket.on('resize', function(data) {
-            term.resize(data.col, data.row);
-        });
-        socket.on('input', function(data) {
-            term.write(data);
-        });
-        socket.on('disconnect', function() {
-            term.end();
-        });
-    })
+    term.on('exit', code => {
+      console.log(`${new Date()} PID=${term.pid} ENDED`);
+      socket.emit('logout');
+    });
+    socket.on('resize', ({ col, row }) => {
+      term.resize(col, row);
+    });
+    socket.on('input', data => {
+      term.write(data);
+    });
+    socket.on('disconnect', () => {
+      term.end();
+    });
+  });
 }
+
+
