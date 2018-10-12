87e716b2795f20d60a8383c4970e3d581bf4bcf2
mchen6 <seeds.c@gmail.com>
2018-10-13 00:03:13 +0800

    being able to lookup service from child thread under worker thread mode

--- app-sandbox.js
+++ app-sandbox.js
@@ -19,9 +19,14 @@ var mm = new ModuleManager();
 //device manager instance is created inside cdifInterface
 var ci = new CdifInterface(mm);
 
+var dm = ci.deviceManager;
 
-var WorkerMessage = require('./lib/worker-message');
-var workerMessage = new WorkerMessage(null);
+//use device-manager's workerMessage instance to receive message from parent
+//child thread should exactly have only this ONE workerMessage instance
+var wm = dm.workerMessage;
+
+// var WorkerMessage = require('./lib/worker-message');
+// var workerMessage = new WorkerMessage(null);
 
 if (!isMainThread) {
   parentPort.on('message', function(msg) {
@@ -31,36 +36,36 @@ if (!isMainThread) {
         // or else main thread will recursively run the load module path
         msg.options.workerThread = false;
         options.setOptions(msg.options);
-        return workerMessage.sendMessageToParent(msg.id, null, null);
+        return wm.sendMessageToParent(msg.id, null, null);
         break;
       }
       case 'load-module': {
         mm.loadModuleFromPath(msg.path, msg.name, msg.version, function(err, mi) {
-          return workerMessage.sendMessageToParent(msg.id, err, null);
+          return wm.sendMessageToParent(msg.id, err, null);
         });
         break;
       }
       case 'invoke-action': {
         ci.invokeDeviceAction(msg.deviceID, msg.serviceID, msg.actionName, msg.args, null, function(err, data) {
-          return workerMessage.sendMessageToParent(msg.id, err, data);
+          return wm.sendMessageToParent(msg.id, err, data);
         });
         break;
       }
       case 'get-spec': {
         ci.getDeviceSpec(msg.deviceID, null, function(err, data) {
-          return workerMessage.sendMessageToParent(msg.id, err, data);
+          return wm.sendMessageToParent(msg.id, err, data);
         });
         break;
       }
       case 'get-schema': {
         ci.getDeviceSchema(msg.deviceID, msg.path, null, function(err, data) {
-          return workerMessage.sendMessageToParent(msg.id, err, data);
+          return wm.sendMessageToParent(msg.id, err, data);
         });
         break;
       }
       case 'invoke-device-callback': {
         ci.invokeDeviceCallbacks(msg.deviceID, msg.path, msg.data, null, function(err, data) {
-          return workerMessage.sendMessageToParent(msg.id, err, data);
+          return wm.sendMessageToParent(msg.id, err, data);
         });
         break;
       }
@@ -68,10 +73,27 @@ if (!isMainThread) {
         ci.discoverAll(function() {
           setTimeout(function() {
             ci.stopDiscoverAll(function() {
-              return workerMessage.sendMessageToParent(msg.id, null, null);
+              return wm.sendMessageToParent(msg.id, null, null);
             });
           }, 5000);
         });
+        break;
+      }
+      case 'query-device-reply': {
+        // message contains: {msgID: msgID, errMsg: errMsg, spec: spec}
+        var id = msg.msgID;
+
+        if (wm.msgQueue[id] != null) {
+          var callback = wm.msgQueue[id];
+          if (callback != null && typeof(callback) === 'function') {
+            if (msg.errMsg != null) {
+              callback(new Error(msg.errMsg), null);
+            } else {
+              callback(null, msg.spec);
+            }
+            delete wm.msgQueue[id];
+          }
+        }
         break;
       }
     }
