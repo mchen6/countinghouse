efeead0215183300f3cbb0625f58298b97ea4310
miaobo chen <mchen@apemesh.com>
2019-01-16 14:51:38 +0800

    add timeout test case

--- test/test3.js
+++ test/test3.js
@@ -293,7 +293,7 @@ describe('test9: invoke specify incorrect input type', function() {
     .expect('Content-Type', /[json | text]/)
     .expect(500, function(err, res) {
       if (err) return done(err);
-      console.log('Response: ' + JSON.stringify(res.body));
+
       if (res.body.message.startsWith('输入数据校验错误') === false || res.body.fault.reason !== '数据不是object类型') {
         console.error(chalk.white.bgRed.bold('Request:' + JSON.stringify(req)));
         console.error(chalk.white.bgRed.bold('Response: ' + JSON.stringify(res.body)));
@@ -370,6 +370,26 @@ describe('test12: invoke specify input validation', function() {
   });
 });
 
+describe('test13: invoke with timeout', function() {
+  this.timeout(0);
+  var req = { serviceID: 'urn:apemesh-com:serviceID:timeOutTestService', actionName: 'testTimeout', input: {} };
+
+  it('invoke with timeout', function(done) {
+    request(url).post('/devices/b752c14b-27ec-5374-a2ca-0ce71c247566/invoke-action')
+    .send(req)
+    .expect('Content-Type', /[json | text]/)
+    .expect(500, function(err, res) {
+      if (err) return done(err);
+
+      if (res.body.message !== '设备无响应') {
+        console.error(chalk.white.bgRed.bold('Request:' + JSON.stringify(req)));
+        console.error(chalk.white.bgRed.bold('Response: ' + JSON.stringify(res.body)));
+        return done(new Error('test13 invoke with timeout fail'));
+      }
+      return done();
+    });
+  });
+});
 
 function testInvokeActions(deviceID, serviceID, serviceList, callback) {
   var actionList = serviceList[serviceID].actionList;
@@ -379,6 +399,9 @@ function testInvokeActions(deviceID, serviceID, serviceList, callback) {
   var list = Object.keys(actionList);
 
   async.eachSeries(list, function(name, cb) {
+    //skip testTimeout API which purposely test timeout scenario and was made as an independent test case
+    if (serviceID === 'urn:apemesh-com:serviceID:timeOutTestService' && name === 'testTimeout') return cb();
+
     setTimeout(function() {
       var action = actionList[name];
       action.should.be.an.Object;
@@ -458,8 +481,8 @@ function testInvokeActions(deviceID, serviceID, serviceList, callback) {
 
           if (deviceID === 'b752c14b-27ec-5374-a2ca-0ce71c247566') {
             if (JSON.stringify(req.input) !== JSON.stringify(res.body.output)) {
-              console.log(chalk.white.bgRed.bold('Request:' + JSON.stringify(req)));
-              console.log(chalk.white.bgRed.bold('Response: ' + JSON.stringify(res.body)));
+              console.error(chalk.white.bgRed.bold('Request:' + JSON.stringify(req)));
+              console.error(chalk.white.bgRed.bold('Response: ' + JSON.stringify(res.body)));
               return cb(new Error('echo test case failed'));
             }
           }
