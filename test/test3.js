56c2cf28c0e8669c5d6be02b6a293a6338f22062
miaobo chen <mchen@apemesh.com>
2019-01-28 16:59:34 +0800

    add BSON binary input test case

--- test/test3.js
+++ test/test3.js
@@ -462,11 +462,11 @@ describe('test16: invoke error expect unknown error', function() {
 });
 
 
-describe('test17: invoke with BSON input', function() {
+describe('test17: invoke with BSON content-type and JSON text', function() {
   this.timeout(0);
   var req = BSON.serialize({ serviceID: 'urn:apemesh-com:serviceID:echoService', actionName: 'echo', input: {foo: [{item1: '111', item2: false}], bar: '222'} });
 
-  it('invoke with BSON input', function(done) {
+  it('invoke with BSON content-type and JSON text', function(done) {
     request(url).post('/devices/b752c14b-27ec-5374-a2ca-0ce71c247566/invoke-action')
     .set('Content-Type', 'application/bson')
     .send(req)
@@ -479,7 +479,38 @@ describe('test17: invoke with BSON input', function() {
   });
 });
 
+describe('test18: invoke with BSON content-type with binary data', function() {
+  this.timeout(0);
+  var req = BSON.serialize({ serviceID: 'urn:apemesh-com:serviceID:echoService', actionName: 'echo', input: {foo: [{item1: '111', item2: false}], bar: '222', binaryData: Buffer.from('abcdefg')} });
 
+  it('invoke with BSON content-type with binary data', function(done) {
+    request(url).post('/devices/b752c14b-27ec-5374-a2ca-0ce71c247566/invoke-action')
+    .set('Content-Type', 'application/bson')
+    .send(req)
+    .expect('Content-Type', /[json | text]/)
+    .expect(200, function(err, res) {
+      if (err) return done(new Error('test18 fail: ' + err.message));
+
+      if (res.body.output == null
+        || res.body.output.binaryData == null
+        || res.body.output.binaryData.buffer == null
+        || res.body.output.binaryData.buffer['0'] !== 97
+        || res.body.output.binaryData.buffer['1'] !== 98
+        || res.body.output.binaryData.buffer['2'] !== 99
+        || res.body.output.binaryData.buffer['3'] !== 100
+        || res.body.output.binaryData.buffer['4'] !== 101
+        || res.body.output.binaryData.buffer['5'] !== 102
+        || res.body.output.binaryData.buffer['6'] !== 103
+      ) {
+        console.error(chalk.white.bgRed.bold('Request:' + JSON.stringify(BSON.deserialize(req))));
+        console.error(chalk.white.bgRed.bold('Response: ' + JSON.stringify(res.body)));
+        return done(new Error('test18 fail'));
+      }
+
+      return done();
+    });
+  });
+});
 
 function testInvokeActions(deviceID, serviceID, serviceList, callback) {
   var actionList = serviceList[serviceID].actionList;
