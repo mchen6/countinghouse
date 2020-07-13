8d371aaea807fee177f73c853dea6fb5b98c553c
mchen6 <seeds.c@gmail.com>
2020-07-13 01:21:47 -0700

    update test case for api log

--- test/unit/test25.js
+++ test/unit/test25.js
@@ -16,19 +16,23 @@ describe('test25: test API log feature', function() {
   var req = { serviceID: 'urn:apemesh-com:serviceID:echoService', actionName: 'echo', input: { foo: [], bar: 'vv'} };
 
   it('invoke should write API log to redis', function(done) {
-    request(url).post('/devices/b752c14b-27ec-5374-a2ca-0ce71c247566/invoke-action')
-    .set('X-Apemesh-Key', 'aabbcc')
-    .send(req)
-    .expect('Content-Type', /[json | text]/)
-    .expect(500, function(err, res) {
+    var beforeLen = 0, afterLen = 0;
+    redisClient.llen('list:aabbcc#b752c14b-27ec-5374-a2ca-0ce71c247566#urn:apemesh-com:serviceID:echoService#echo', function(err, data) {
       if (err) return done(err);
+      beforeLen = data;
 
-      if (res.body.message.startsWith('输出数据校验错误') === false || res.body.fault.reason !== '未找到输出参数') {
-        console.error(chalk.white.bgRed.bold('Request:' + JSON.stringify(req)));
-        console.error(chalk.white.bgRed.bold('Response: ' + JSON.stringify(res.body)));
-        return done(new Error('test24 fail'));
-      }
-      return done();
+      request(url).post('/devices/b752c14b-27ec-5374-a2ca-0ce71c247566/invoke-action')
+      .set('X-Apemesh-Key', 'aabbcc')
+      .send(req)
+      .expect('Content-Type', /[json | text]/)
+      .expect(200, function(err, res) {
+        if (err) return done(err);
+        redisClient.llen('list:aabbcc#b752c14b-27ec-5374-a2ca-0ce71c247566#urn:apemesh-com:serviceID:echoService#echo', function(err, data) {
+          if (err) return done(err);
+          if ((beforeLen + 1) !== data) return done(new Error('API log length mismatch'));
+          return done();
+        });
+      });
     });
   });
 });
