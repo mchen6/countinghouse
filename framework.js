00fbf62cf77b4d0e827d76240f2ac37fdd8604c3
miaobo chen <mchen@apemesh.com>
2020-09-28 16:50:07 +0800

    preliminary redis api cache support this is to avoid creating redis connection under each worker thread

--- framework.js
+++ framework.js
@@ -48,6 +48,8 @@ global.CdifDevice   = require('./lib/cdif-device');
 global.CdifError    = require('./lib/cdif-error').CdifError;
 global.DeviceError  = require('./lib/cdif-error').DeviceError;
 
+var redisAPICache = require('./lib/redis-api-cache');
+redisAPICache.init();
 
 // routeManager.startServer();
 mm.loadAllModules();
